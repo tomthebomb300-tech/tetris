289c8fad4ae72dd9c337afd96b4206b63fbdd398
Thomas O' Callaghan <tomthebomb300@gmail.com>
2026-07-03 19:53:34 +0100

    Displaying window

--- src/main.c
+++ src/main.c
@@ -1,5 +1,82 @@
 #include <stdio.h>
+#include <windows.h>
 
-int main(){
-    printf("Working\n");
+const char WINDOW_CLASS_NAME[] = "tetris_window";
+
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 800;
+
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam){
+    switch(msg){
+        case WM_KEYDOWN:
+            break;
+        case WM_CLOSE:
+            DestroyWindow(hwnd);
+            break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        default:
+            return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
+    WNDCLASSEX wc;
+    MSG Msg;
+
+    HBRUSH backgroundBrush = CreateSolidBrush(RGB(0,0,0));
+
+    //Registering the Window Class
+    wc.cbSize        = sizeof(WNDCLASSEX);
+    wc.style         = 0;
+    wc.lpfnWndProc   = WndProc;
+    wc.cbClsExtra    = 0;
+    wc.cbWndExtra    = 0;
+    wc.hInstance     = hInstance;
+    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = backgroundBrush;
+    wc.lpszMenuName  = NULL;
+    wc.lpszClassName = WINDOW_CLASS_NAME;
+    wc.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
+
+    if(!RegisterClassEx(&wc)){
+        MessageBox(NULL, "Window Registration Failed!", "Error!",
+            MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+    //Creating the Window
+    HWND hwnd = CreateWindowEx(
+        WS_EX_CLIENTEDGE,
+        WINDOW_CLASS_NAME,
+        "Tetris",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, WINDOW_WIDTH, WINDOW_HEIGHT,
+        NULL, NULL, hInstance, NULL);
+
+    if(hwnd == NULL){
+        MessageBox(NULL, "Window Creation Failed!", "Error!",
+            MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+
+    int run = 1;
+    while(run){
+        //The Message Loop
+        while(PeekMessage(&Msg, NULL, 0, 0, PM_REMOVE)){
+            if(Msg.message == WM_QUIT)
+                run = 0;
+            TranslateMessage(&Msg);
+            DispatchMessage(&Msg);
+        }
+    }
+    DeleteObject(backgroundBrush);
+    return Msg.wParam;
 }
